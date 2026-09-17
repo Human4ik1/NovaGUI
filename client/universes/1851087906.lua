5b42f95dfb06fbdc5fe1bbaf2929299fa5b2976f
Илья Кряжик <Human4ikman4ik@gmail.com>
2026-09-18 03:35:54 +0500

    Create hunting game universe redirect script
    Added a new Lua script for hunting game universe redirection.

---
+++ client/universes/1851087906.lua
@@ -0,0 +1,8 @@
+--[[
+  Hunting game universe redirect (UniverseId 1851087906).
+  Repo path: client/universes/1851087906.lua
+]]
+
+local MODULE_URL = "https://raw.githubusercontent.com/Human4ik1/NovaGUI/main/client/places/120073741652089.lua"
+
+return loadstring(game:HttpGet(MODULE_URL))()
