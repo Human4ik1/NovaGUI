c9ef71f76c4fd08a7f04113461f8184d880e9723
Илья Кряжик <Human4ikman4ik@gmail.com>
2026-09-18 04:16:37 +0500

    Create 6331902150.lua

---
+++ client/universes/6331902150.lua
@@ -0,0 +1,8 @@
+--[[
+  Forsaken universe redirect (UniverseId 6331902150).
+  Repo path: client/universes/6331902150.lua
+]]
+
+local MODULE_URL = "https://raw.githubusercontent.com/Human4ik1/NovaGUI/main/client/places/83645629621104.lua"
+
+return loadstring(game:HttpGet(MODULE_URL))()
