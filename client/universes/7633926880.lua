509672254d395c2ed4be698e81d182c436b66f34
Илья Кряжик <Human4ikman4ik@gmail.com>
2026-09-18 01:58:13 +0500

    Create 7633926880.lua

---
+++ client/universes/7633926880.lua
@@ -0,0 +1,10 @@
+--[[
+  Tactical shooter universe redirect (UniverseId 7633926880).
+  Repo path: client/universes/7633926880.lua
+
+  Every map of the game funnels into the single maintained module.
+]]
+
+local MODULE_URL = "https://raw.githubusercontent.com/Human4ik1/NovaGUI/main/client/places/114234929420007.lua"
+
+return loadstring(game:HttpGet(MODULE_URL))()
