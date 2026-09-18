be9354a3b49d8f4cb85514d61902ed2e4f135179
Илья Кряжик <Human4ikman4ik@gmail.com>
2026-09-18 05:23:25 +0500

    Create 10766457017.lua

---
+++ client/universes/10766457017.lua
@@ -0,0 +1,8 @@
+--[[
+  Destroy the Cube universe redirect (UniverseId 10766457017).
+  Repo path: client/universes/10766457017.lua
+]]
+
+local MODULE_URL = "https://raw.githubusercontent.com/Human4ik1/NovaGUI/main/client/places/124745235987813.lua"
+
+return loadstring(game:HttpGet(MODULE_URL))()
