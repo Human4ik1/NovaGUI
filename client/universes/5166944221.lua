f677cfaa84c88d019cf8729d5191380662e79495
HumaHub <humahub.lua@gmail.com>
2026-09-19 23:30:13 +0300

    Create 5166944221.lua

---
+++ client/universes/5166944221.lua
@@ -0,0 +1,10 @@
+--[[
+  Death Ball universe redirect (UniverseId 5166944221).
+  Repo path: client/universes/5166944221.lua
+
+  Every map of the game funnels into the single maintained module.
+]]
+
+local MODULE_URL = "https://raw.githubusercontent.com/Human4ik1/NovaGUI/main/client/places/83678792452277.lua"
+
+return loadstring(game:HttpGet(MODULE_URL))()
