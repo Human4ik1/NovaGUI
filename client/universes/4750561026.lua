eefb2ad46b2bb69ecd7e4a90ffe2643ea3271f65
HumaHub <humahub.lua@gmail.com>
2026-09-19 20:46:25 +0300

    Create 4750561026.lua

---
+++ client/universes/4750561026.lua
@@ -0,0 +1,10 @@
+--[[
+  Cold War universe redirect (UniverseId 4750561026).
+  Repo path: client/universes/4750561026.lua
+
+  Every map of the game funnels into the single maintained module.
+]]
+
+local MODULE_URL = "https://raw.githubusercontent.com/Human4ik1/NovaGUI/main/client/places/13687899540.lua"
+
+return loadstring(game:HttpGet(MODULE_URL))()
